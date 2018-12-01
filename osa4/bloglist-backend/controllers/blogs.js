03aea0d3c3537ba06f17a810696120b2594c04ab
Matti Riekkinen <matti.riekkinen@icloud.com>
2018-12-01 17:02:12 +0200

    Tehtävä 4.19

--- osa4/bloglist-backend/controllers/blogs.js
+++ osa4/bloglist-backend/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   try {
@@ -14,9 +15,24 @@ blogsRouter.get('/', async (request, response) => {
   }
 })
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   try {
     const body = request.body
+
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
 
     if (!body.title) {
       return response.status(400).json({ error: 'title missing' })
@@ -26,9 +42,7 @@ blogsRouter.post('/', async (request, response) => {
       return response.status(400).json({ error: 'URL missing' })
     }
 
-    // This will change at a later time
-    const users = await User.find({})
-    const user = users[0]
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       title: body.title,
@@ -45,7 +59,12 @@ blogsRouter.post('/', async (request, response) => {
 
     response.status(201).json(Blog.format(savedBlog))
   } catch (exception) {
-    response.status(500).json({ error: 'something went wrong...' })
+    if (exception.name === 'JsonWebTokenError' ) {
+      response.status(401).json({ error: exception.message })
+    } else {
+      console.log(exception)
+      response.status(500).json({ error: 'something went wrong...' })
+    }
   }
 })
 
