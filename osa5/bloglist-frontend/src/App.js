2f6b57b20fe26652add1d4527621ecae23e94a23
Matti Riekkinen <matti.riekkinen@icloud.com>
2018-12-03 20:55:08 +0200

    Tehtävä 5.1

---
+++ osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import Blog from './components/Blog'
+import Notification from './components/Notification'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      blogs: [],
+      error: null,
+      username: '',
+      password: '',
+      user: null
+    }
+  }
+
+  componentDidMount() {
+    blogService.getAll().then(blogs =>
+      this.setState({ blogs })
+    )
+  }
+
+  login = async (event) => {
+    event.preventDefault()
+    try{
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+
+      this.setState({ username: '', password: '', user})
+    } catch(exception) {
+      this.setState({
+        error: 'invalid username or password',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
+    }
+  }
+
+  handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  render() {
+    if (this.state.user === null) {
+      return (
+        <div>
+        <h2>Log in to application</h2>
+
+        <Notification message={this.state.error} />
+
+        <form onSubmit={this.login}>
+          <div>
+            username:
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <div>
+            password:
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+      )
+    }
+
+    return (
+      <div>
+        <h2>blogs</h2>
+        <Notification message={this.state.error} />
+        <p>{this.state.user.name} logged in</p>
+        {this.state.blogs.map(blog => 
+          <Blog key={blog.id} blog={blog}/>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
