73e0a0f0b13ebe8f58b73aa6193164fb18356b54
Matti Riekkinen <matti.riekkinen@icloud.com>
2018-12-04 18:22:06 +0200

    Tehtävä 5.6

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,59 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      blog: props.blog,
+      isBig: false
+    }
+  }
+
+  blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  detailsStyle = {
+    paddingLeft: 10
+  }
+
+  handleClick = () => {
+    this.setState({ isBig: !this.state.isBig })
+  }
+
+  getName = (user) => {
+    if (!user) {
+      return ''
+    }
+    return user.name
+  }
+
+  render() {
+    if (this.state.isBig) {
+      return (
+        <div style={this.blogStyle}>
+          <div onClick={this.handleClick}>
+            {this.state.blog.title} {this.state.blog.author}
+          </div>
+          <div style={this.detailsStyle}>
+            <a href={this.state.blog.url}>{this.state.blog.url}</a><br/>
+            {this.state.blog.likes} likes <button>like</button><br/>
+            added by {this.getName(this.state.blog.user)}
+          </div>
+        </div>
+      )
+    }
+
+    return (
+      <div style={this.blogStyle} onClick={this.handleClick}>
+        {this.state.blog.title} {this.state.blog.author}
+      </div>
+    )
+  }
+}
 
 export default Blog
