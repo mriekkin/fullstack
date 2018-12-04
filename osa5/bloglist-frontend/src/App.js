a5490388dad1ad0b5a524652261a4e150e2be1ce
Matti Riekkinen <matti.riekkinen@icloud.com>
2018-12-04 17:22:08 +0200

    Tehtävä 5.5

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Blog from './components/Blog'
 import Notification from './components/Notification'
 import CreateBlog from './components/CreateBlog'
+import Togglable from './components/Togglable'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -58,6 +59,7 @@ class App extends React.Component {
   }
 
   addBlog = (newBlog) => {
+    this.createBlogForm.toggleVisibility()
     this.showNotification(`a new blog '${newBlog.title}' by ${newBlog.author} added`)
     this.setState({ blogs: this.state.blogs.concat(newBlog) })
   }
@@ -116,10 +118,12 @@ class App extends React.Component {
           {this.state.user.name} logged in
           <button onClick={this.logout}>logout</button>
         </p>
-        <CreateBlog
-          addBlog={this.addBlog}
-          showNotification={this.showNotification}
-        />
+        <Togglable buttonLabel="new blog" ref={component => this.createBlogForm = component}>
+          <CreateBlog
+            addBlog={this.addBlog}
+            showNotification={this.showNotification}
+          />
+        </Togglable>
         {this.state.blogs.map(blog => 
           <Blog key={blog.id} blog={blog}/>
         )}
