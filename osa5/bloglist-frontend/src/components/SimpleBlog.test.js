026085b02458683f37e1f117791e11b4c7b8feb3
Matti Riekkinen <matti.riekkinen@icloud.com>
2018-12-06 13:59:42 +0200

    Tehtävä 5.13

--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -10,12 +10,35 @@ describe('<SimpleBlog />', () => {
       likes: 10
     }
 
-    const noteComponent = shallow(<SimpleBlog blog={blog} />)
-    const titleDiv = noteComponent.find('.title')
-    const likesDiv = noteComponent.find('.likes')
+    const blogComponent = shallow(<SimpleBlog blog={blog} />)
+    const titleDiv = blogComponent.find('.title')
+    const likesDiv = blogComponent.find('.likes')
 
     expect(titleDiv.text()).toContain(blog.title)
     expect(titleDiv.text()).toContain(blog.author)
     expect(likesDiv.text()).toContain('blog has 10 likes')
+  })
+
+  it('clicking the like button twice calls the event handler twice', () => {
+    const blog = {
+      title: 'A simple blog',
+      author: 'John Doe',
+      likes: 10
+    }
+
+    const mockHandler = jest.fn()
+
+    const blogComponent = shallow(
+      <SimpleBlog
+        blog={blog}
+        onClick={mockHandler}
+      />
+    )
+
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
   })
 })
