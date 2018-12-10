050b54a7903b33304bfb8366db22ac73573ac861
Matti Riekkinen <matti.riekkinen@icloud.com>
2018-12-10 18:25:50 +0200

    Tehtävä 6.2

--- osa6/redux-anecdotes-v2/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes-v2/src/components/AnecdoteList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { anecdoteVoting } from '../reducers/anecdoteReducer'
 
 class AnecdoteList extends React.Component {
   render() {
@@ -13,8 +14,10 @@ class AnecdoteList extends React.Component {
             </div>
             <div>
               has {anecdote.votes}
-              <button onClick={() => 
-                this.props.store.dispatch({ type: 'VOTE', id: anecdote.id })
+              <button onClick={() =>
+                this.props.store.dispatch(
+                  anecdoteVoting(anecdote.id)
+                )
               }>
                 vote
               </button>
